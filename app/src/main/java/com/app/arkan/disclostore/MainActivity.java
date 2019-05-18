a492158aa1380f75d78c54cc7916237c30292ea1
Arkan Jbali <arkan.1997@gmail.com>
2019-05-18 23:01:36 +0300

    Initial Commit

--- app/src/main/java/com/app/arkan/disclostore/MainActivity.java
+++ app/src/main/java/com/app/arkan/disclostore/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//Arkan Jbali
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
