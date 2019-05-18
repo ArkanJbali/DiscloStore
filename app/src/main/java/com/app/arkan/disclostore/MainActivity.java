1e3345725d5b7ac267af895626b67965a4966dde
Arkan Jbali <arkan.1997@gmail.com>
2019-05-19 00:18:45 +0300

    Arkan Commit

--- app/src/main/java/com/app/arkan/disclostore/MainActivity.java
+++ app/src/main/java/com/app/arkan/disclostore/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//Arkan Jbali
+//Arkan Jbali2
 
 //Suleiman AbdElnabi
     //Mostfa Maree
